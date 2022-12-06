233916b9348b1826f5958e50b84541b7e327a1d7
arielniscola <arielniscola@gmail.com>
2022-12-06 13:03:40 -0300

    C:/Program Files/Git/routes create turn route

---
+++ src/routes/Turn.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+
+
+const turnRoutes = Router();
+
+turnRoutes.get('/', (req, res) => {});
+turnRoutes.get('/:id', (req, res) => {});
+turnRoutes.post('/', (req, res) => {});
+
+
+module.exports = turnRoutes;
