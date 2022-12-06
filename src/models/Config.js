76124e1604f020b29a84f9a1d6a57c11c07bdc5e
Maximiliano <maxi950live@gmail.com>
2022-12-06 11:30:19 -0300

    se agrego el modelo config

---
+++ src/models/Config.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+    sequelize.define('config', {
+    id:{
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        allowNull: false,
+        primaryKey: true
+    },
+    open_days: {
+        type: DataTypes.ENUM('lunes', 'martes', 'miercoles', 'jueves', 'viernes', 'sabado', 'domingo'),
+        allowNull: false,
+    },
+    duration_turno:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    price_turno:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    }
+    });
+};
