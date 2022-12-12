7e889439a4560c9a89d67490ce68d86c70ec51c0
maestrejorge <maestrejorgeluis@hotmail.com>
2022-12-12 07:52:00 -0500

    prueba

--- src/controllers/court.controller.js
+++ src/controllers/court.controller.js
@@ -29,14 +29,9 @@ const getCourtID = async (req, res) => {
 
 const updateCourt = async (req, res) => {
   try {
-<<<<<<< HEAD
-    const data = await courtService.updateCourt(req.body, req.params.id);
-    res.status(201).json(data);
-=======
     const {id} = req.params;
     courtService.updateCourt(id, req.body);
     res.send('Court updated successfully');
->>>>>>> a8ea5b51ee706e7df390389843d99ee5d544b67b
   } catch (error) {
     res.status(400).json(data);
   }
@@ -44,13 +39,8 @@ const updateCourt = async (req, res) => {
 
 const deleteCourt = async (req, res) => {
   try {
-<<<<<<< HEAD
-    const data = await courtService.deleteCourt(req.params.id);
-    res.status(200).json(data);
-=======
     await courtService.deleteCourt(req.params.id);
     res.send('Court deleted successfully');
->>>>>>> a8ea5b51ee706e7df390389843d99ee5d544b67b
   } catch (error) {
     res.status(400).json(error);
   }
--- src/services/court.service.js
+++ src/services/court.service.js
@@ -8,17 +8,12 @@ const getAllCourt = async () => {
 
 const createCourt = async (data) => {
   const { numberCourt, description, typeCourt } = data;
-<<<<<<< HEAD
-  if (!numberCourt && !description && !typeCourt) throw "Required data missing";
-  const newCourt = await Court.create(data);
-=======
   if (!numberCourt) throw "Required data missing";
   const newCourt = await Court.create({
     numberCourt: data.numberCourt,
     description: data.description,
     typeCourt: data.typeCourt
   });
->>>>>>> a8ea5b51ee706e7df390389843d99ee5d544b67b
   if (!newCourt) throw "Object no create";
   return newCourt;
 };
@@ -30,28 +25,6 @@ const getCourtID = async (id) => {
   return data;
 };
 
-<<<<<<< HEAD
-const updateCourt = async (obj, id) => {
-  const { numberCourt, description, typeCourt } = obj;
-  if (!id) throw "no ID especified";
-  if (numberCourt && description && typeCourt) throw "Required data missing";
-  const court = await Court.findByPk(id);
-  court.numberCourt = numberCourt;
-  court.description = description;
-  court.typeCourt = typeCourt;
-  await court.save();
-  return court;
-};
-
-const deleteCourt = async (id) => {
-  if (!id) throw "no ID especified";
-  const courtDelete = await Court.destroy({
-    where: {
-      id,
-    },
-  });
-  return courtDelete;
-=======
 const updateCourt = async (id, data) => {
   try {
     const {numberCourt, description, typeCourt} = data; 
@@ -73,7 +46,6 @@ const deleteCourt = async (id) => {
         id,
     },
 });
->>>>>>> a8ea5b51ee706e7df390389843d99ee5d544b67b
 };
 
 module.exports = {
