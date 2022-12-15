b1b9fcd08e6bd901ba1c1fb7af9ccbd4a2663e47
arielniscola <41024148+arielniscola@users.noreply.github.com>
2022-12-15 13:15:37 -0300

    Update index.js
    cambio variable de entorno puerto

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require("dotenv").config();
 
+const port = process.env.PORT || 3001;
 
 conn.sync().then(() => {
-  server.listen(3001, async () => {
+  server.listen(port, async () => {
     console.log('%s listening at 3001'); 
   });
 });
