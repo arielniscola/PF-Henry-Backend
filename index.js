82b128d800e8f89da8e88f317f459092ddb1d80d
arielniscola <41024148+arielniscola@users.noreply.github.com>
2022-12-15 08:36:03 -0300

    Update index.js
    No cambiar la configuracion

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
 
-conn.sync({force: true}).then(() => {
+conn.sync().then(() => {
   server.listen(3001, async () => {
     console.log('%s listening at 3001'); 
   });
