8a1ba34fcb3b4a1478ccc7005d2a68a06cf57aab
Braian Gomez <braiandenisgomez@gmail.com>
2022-12-16 01:02:01 -0300

    asdasd

--- src/models/Client.js
+++ src/models/Client.js
@@ -1,32 +1,69 @@
-const { DataTypes } = require('sequelize');
+const bcrypt = require("bcrypt");
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-    sequelize.define('client', {
-    id:{
+  sequelize.define(
+    "client",
+    {
+      id: {
         type: DataTypes.UUID,
         defaultValue: DataTypes.UUIDV4,
         allowNull: false,
-        primaryKey: true
-    },
-    name: {
+        primaryKey: true,
+      },
+      name: {
         type: DataTypes.STRING,
+        defaultValue: "",
+      },
+      email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+      },
+      direction: {
+        type: DataTypes.STRING,
+        defaultValue: "",
+      },
+      dni: {
+        type: DataTypes.STRING,
+        defaultValue: "",
+      },
+      country: {
+        type: DataTypes.STRING,
+        defaultValue: "",
+      },
+      profile_img: {
+        type: DataTypes.STRING,
+        defaultValue: "",
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      token: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      isActive: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      isOwner: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      }
     },
-    celNumber:{
-        type: DataTypes.INTEGER,
-        allowNull: false
-    },
-    direction: {
-        type: DataTypes.STRING
-    },
-    dni:{
-        type: DataTypes.INTEGER
-    },
-    country:{
-        type: DataTypes.STRING
+    {
+      timestamps: false,
+      hooks: {
+        beforeCreate: async (client) => {
+          const salt = await bcrypt.genSalt(10);
+          client.password = await bcrypt.hash(client.password, salt);
+        },
+      },
     }
-    },{
-        timestamps: false
-    });
+  );
 };
+
