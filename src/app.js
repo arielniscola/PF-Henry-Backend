8ebe3e075c1b50eb3e4eefa6e3c8155ee4005a51
arielniscola <41024148+arielniscola@users.noreply.github.com>
2023-01-08 20:42:52 -0300

    Update URL CORS

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); 
+  res.header('Access-Control-Allow-Origin', 'https://deploy-pf.vercel.app/'); 
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
