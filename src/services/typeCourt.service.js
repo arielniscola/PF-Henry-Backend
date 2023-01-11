c71fcde70c7aaa679416629a1f67c8c832e21438
arielniscola <arielniscola@gmail.com>
2023-01-11 16:06:19 -0300

    cloudinary icon typecourt

--- src/services/typeCourt.service.js
+++ src/services/typeCourt.service.js
@@ -1,57 +1,61 @@
 const { TypeCourt } = require("../db");
 
-const getAllTypeCourt = async()=>{
-    const data = await TypeCourt.findAll();
-    if(!data) throw "Data not found"
-    return data
-}
+const getAllTypeCourt = async () => {
+  const data = await TypeCourt.findAll();
+  if (!data) throw "Data not found";
+  return data;
+};
 
-const createTypeCourt = async(data)=>{
-    const { description } = data;
-    if(!description) throw "Data required missing"
+const createTypeCourt = async (data) => {
+  const { description, icon } = data;
+  if (!description) throw "Data required missing";
 
-    const newTypeCourt = await TypeCourt.create(data);
+  const imageUpload = await cloudinary.uploader.upload(icon, {
+    folder: "henry",
+    upload_preset: "ml_default",
+  });
 
-    if(!newTypeCourt) throw "Type Court no created"
+  const newTypeCourt = await TypeCourt.create({ description, imageUpload });
 
-    return newTypeCourt
-}
+  if (!newTypeCourt) throw "Type Court no created";
+
+  return newTypeCourt;
+};
 
 const getTypeCourtID = async (id) => {
-    const typeCourt = await TypeCourt.findByPk(id);
+  const typeCourt = await TypeCourt.findByPk(id);
 
-    if(!typeCourt) throw "Not found"
-    return typeCourt
-}   
+  if (!typeCourt) throw "Not found";
+  return typeCourt;
+};
 
-const deleteTypeCourt = async(id) =>{
-    const deleted = await TypeCourt.destroy({
-        where:{
-            id: id
-        },
-    });
-    return deleted;
-}
+const deleteTypeCourt = async (id) => {
+  const deleted = await TypeCourt.destroy({
+    where: {
+      id: id,
+    },
+  });
+  return deleted;
+};
 
-const updateTypeCourt = async (id, data) =>{
-    try {
-        const {description, icon} = data; 
+const updateTypeCourt = async (id, data) => {
+  try {
+    const { description, icon } = data;
 
-        const typecourt = await TypeCourt.findByPk(id);
-        typecourt.description = description;
-        typecourt.icon = icon;
+    const typecourt = await TypeCourt.findByPk(id);
+    typecourt.description = description;
+    typecourt.icon = icon;
 
-        await typecourt.save();
-    } catch (error) {
-        res.status(400).json(error)
-    }
-}
-
+    await typecourt.save();
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
 
 module.exports = {
-    getAllTypeCourt,
-    createTypeCourt,
-    getTypeCourtID,
-    deleteTypeCourt,
-    updateTypeCourt
-}
+  getAllTypeCourt,
+  createTypeCourt,
+  getTypeCourtID,
+  deleteTypeCourt,
+  updateTypeCourt,
+};
