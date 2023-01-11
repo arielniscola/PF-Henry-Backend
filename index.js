aeb8715f10f3fe2fcf58db990a12e59c7d619ab2
Jorge Maestre <maestrejorgeluis@hotmail.com>
2023-01-11 00:44:51 -0500

    tienes que tener el force true para que no se rompa

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const {createUsersBk} = require("./src/services/client.service.js")
 
 const port = process.env.PORT || 3001;
 
-conn.sync().then(async () => {
+conn.sync({force:true}).then(async () => {
   server.listen(port, async () => {
     console.log('%s listening at 3001'); 
     await createUsersBk()
